28bba66cbe170f834aac3e92654dec3f458fda09
Andreas Stenhall <andreas.stenhall@coligo.se>
2025-02-10 22:04:47 +0100

    Create Remediate-KeyboardLayout.ps1

---
+++ Set-KeyboardLayout/Remediate-KeyboardLayout.ps1
@@ -0,0 +1,11 @@
+<#
+Version: 1.0
+Author: Andreas Stenhall - Trusted Advisor at Coligo - andreas.stenhall@coligo.se
+Script: RemediateKeyboardLayout.ps1
+Description: Set Swedish(sv-SE)  keyboard layout only and make sure that the Windows Display Language is still set to English (United States) (en-US).
+Run as: User
+Context: 64-bit
+#> 
+
+Set-WinUserLanguageList sv-SE -Force
+Set-WinUILanguageOverride -Language en-US
